ae49ef1bb83d24fa5bd47b220cec2306ea8b86b0
Daniel Beyon <76236947+byunboy2@users.noreply.github.com>
2023-02-18 21:29:13 -0800

    Time: 86 ms (79.25%), Space: 45.5 MB (63.86%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -1,47 +1,18 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
+const buildTree= (preorderTraversal, inorderTraversal) => {
+    const inorderIndexMap = new Map(inorderTraversal.map((val, idx) => [val, idx]));
 
-/**
- * @param {number[]} preorder
- * @param {number[]} inorder
- * @return {TreeNode}
- */
-var buildTree = function(preorder, inorder) {
-    // create a map to store the index of each node in the inorder array
-    const indexMap = new Map();
-    for (let i = 0; i < inorder.length; i++) {
-        indexMap.set(inorder[i], i);
-    }
-    
-    // recursive helper function to build the tree
-    const buildTreeHelper = function(preorderStart, preorderEnd, inorderStart, inorderEnd) {
-        if (preorderStart > preorderEnd) {
-            return null;
-        }
+    const buildTreeHelper = (preorderStart, preorderEnd, inorderStart, inorderEnd) => {
+        if (preorderStart > preorderEnd) return null;
         
-        // create the root node using the first node in the preorder array
-        const rootVal = preorder[preorderStart];
-        const root = new TreeNode(rootVal);
-        
-        // find the index of the root node in the inorder array
-        const inorderIndex = indexMap.get(rootVal);
-        
-        // calculate the number of nodes in the left subtree and right subtree
+        const root = new TreeNode(preorderTraversal[preorderStart]);
+        const inorderIndex = inorderIndexMap.get(root.val);
         const leftSubtreeSize = inorderIndex - inorderStart;
-        const rightSubtreeSize = inorderEnd - inorderIndex;
         
-        // recursively build the left subtree and right subtree
         root.left = buildTreeHelper(preorderStart + 1, preorderStart + leftSubtreeSize, inorderStart, inorderIndex - 1);
-        root.right = buildTreeHelper(preorderEnd - rightSubtreeSize + 1, preorderEnd, inorderIndex + 1, inorderEnd);
+        root.right = buildTreeHelper(preorderStart + leftSubtreeSize + 1, preorderEnd, inorderIndex + 1, inorderEnd);
         
         return root;
     };
     
-    return buildTreeHelper(0, preorder.length - 1, 0, inorder.length - 1);
+    return buildTreeHelper(0, preorderTraversal.length - 1, 0, inorderTraversal.length - 1);
 };
