6cdd2dd883e77fb816bb31253ab7572fa0231564
Daniel Beyon <76236947+byunboy2@users.noreply.github.com>
2023-02-24 21:33:22 -0800

    Time: 77 ms (89.29%), Space: 51 MB (92.13%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.js
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.js
@@ -1,18 +1,27 @@
-function maxPathSum(root) {
-  let maxSum = -Infinity;
-  
-  function maxPathSumHelper(node) {
-    if (!node) return 0;
-    
-    const leftSum = Math.max(0, maxPathSumHelper(node.left));
-    const rightSum = Math.max(0, maxPathSumHelper(node.right));
-    
-    maxSum = Math.max(maxSum, leftSum + node.val + rightSum);
-    
-    return Math.max(leftSum, rightSum) + node.val;
-  }
-  
-  maxPathSumHelper(root);
-  
-  return maxSum;
-}
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+    if(!root) return 0
+    let maxSum = -Infinity;
+    function calculateMaxSum(node){
+        if(!node) return 0
+        let leftBranchSum = Math.max(0,calculateMaxSum(node.left));
+        let rightBranchSum = Math.max(0,calculateMaxSum(node.right));
+        
+        maxSum = Math.max(maxSum,leftBranchSum+node.val+rightBranchSum);
+        return Math.max(0,Math.max(leftBranchSum, rightBranchSum)) + node.val
+    }
+    calculateMaxSum(root);
+    return maxSum;
+};
+
