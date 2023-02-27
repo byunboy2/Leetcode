c57b0f66ceaf11cc06d0225ffd710958e182c1f2
Daniel Beyon <76236947+byunboy2@users.noreply.github.com>
2023-02-27 10:08:57 -0800

    Time: 27 ms (88.50%), Space: 13.7 MB (94.40%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,7 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         first_combination,second_combination,third_combination = [1,1,1]
-        for i in range(n-1,0,-1):
+        for i in range(n-1):
             third_combination = first_combination + second_combination
             second_combination = first_combination
             first_combination = third_combination
