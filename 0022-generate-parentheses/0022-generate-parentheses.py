22369e66fc174e6719310d4b2e1cdb77b8f304b4
Daniel Beyon <76236947+byunboy2@users.noreply.github.com>
2023-03-01 22:28:30 -0800

    Time: 34 ms (78.96%), Space: 14.2 MB (69.20%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -2,12 +2,9 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         if not n: return []
         all_combinations = []
-        def backtrack(current_combination,open_count, close_count):
-            if len(current_combination) == 2*n:
-                all_combinations.append(current_combination)
-            if open_count < n:
-                backtrack(current_combination+"(",open_count+1,close_count)
-            if open_count > close_count:
-                backtrack(current_combination+")",open_count,close_count+1)
+        def backtrack(current_combination: str,open_count:int , close_count:int ):
+            if len(current_combination) == 2*n: all_combinations.append(current_combination)
+            if open_count < n: backtrack(current_combination+"(",open_count+1,close_count)
+            if open_count > close_count: backtrack(current_combination+")",open_count,close_count+1)
         backtrack("",0,0)
         return all_combinations
