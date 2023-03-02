aa9614cfb668b644d52a9048e2f4fcc47510f5b6
Daniel Beyon <76236947+byunboy2@users.noreply.github.com>
2023-03-01 20:35:38 -0800

    Time: 64 ms (75.47%), Space: 44.5 MB (12.34%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.ts
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.ts
@@ -1,18 +1,20 @@
 function letterCombinations(digits: string): string[] {
-  if (!digits) return [];
-  const all_combinations = [];
-  let combination = "";
-  const numberToLetterMapping = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
-  const recursivelyBacktrack = (currentDigit: string, currentCombination: string) => {
-    if (!currentDigit) {
-      all_combinations.push(currentCombination);
-      return;
+    if(!digits) return [];
+    const all_combinations = [];
+    let combination = "";
+    const numberToLetterMapping = ["","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"];
+    const recursivelyBacktrack=(currentDigit:string, currentCombination:string)=>{
+        if(!currentDigit){
+            all_combinations.push(currentCombination);
+            return
+        }
+       for(let letter of numberToLetterMapping[ Number(currentDigit[0])]){
+            let newCombination = currentCombination + letter;
+            recursivelyBacktrack(currentDigit.slice(1),newCombination);
+        }
     }
-    for (const letter of numberToLetterMapping[Number(currentDigit[0])]) {
-      const newCombination = currentCombination + letter;
-      recursivelyBacktrack(currentDigit.slice(1), newCombination);
-    }
-  };
-  recursivelyBacktrack(digits, combination);
-  return all_combinations;
-}
+    recursivelyBacktrack(digits,combination);
+    return all_combinations;
+};
+
+
