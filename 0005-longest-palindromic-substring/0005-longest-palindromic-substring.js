480029a27b5305daa75b6938d6990f3916fedaf5
Daniel Beyon <76236947+byunboy2@users.noreply.github.com>
2023-03-11 10:41:17 -0800

    Time: 3917 ms (12.83%), Space: 71.5 MB (7.70%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -1,24 +1,22 @@
 /**
- * @param {string} s
- * @return {string}
- */
+
+@param {string} s
+@return {string}
+*/
 function longestPalindrome(s) {
-  const n = s.length;
-  let maxLen = 0;
-  let start = 0;
-  const dp = Array.from({ length: n }, () => Array.from({ length: n }, () => false));
-  
-  for (let i = n - 1; i >= 0; i--) {
-    for (let j = i; j < n; j++) {
-      if (s[i] === s[j] && (j - i <= 1 || dp[i+1][j-1])) {
-        dp[i][j] = true;
-        if (j - i + 1 > maxLen) {
-          maxLen = j - i + 1;
-          start = i;
-        }
-      }
-    }
-  }
-  
-  return s.slice(start, start + maxLen);
+    const length = s.length;
+    let maxLength = 0;
+    let startingIndex = 0;
+    const dp = Array.from({ length }, () => Array.from({ length }, () => false));
+    
+    for (let i = length - 1; i >= 0; i--) {
+    for (let j = i; j < length; j++) {
+    if (s[i] === s[j] && (j - i <= 1 || dp[i+1][j-1])) {
+    dp[i][j] = true;
+    if (j - i + 1 > maxLength) {
+    maxLength = j - i + 1;
+    startingIndex = i;
+    }}}}
+    
+    return s.slice(startingIndex, startingIndex + maxLength);
 }
