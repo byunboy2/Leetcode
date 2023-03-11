02aceaf2ec56a0276002e8e3133c4174acb7f46c
Daniel Beyon <76236947+byunboy2@users.noreply.github.com>
2023-03-11 10:54:31 -0800

    Time: 3952 ms (12.77%), Space: 72.2 MB (6.62%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -8,15 +8,14 @@ function longestPalindrome(s) {
     let maxLength = 0;
     let startingIndex = 0;
     const dp = Array.from({ length }, () => Array.from({ length }, () => false));
-    
-    for (let i = length - 1; i >= 0; i--) {
-    for (let j = i; j < length; j++) {
-    if (s[i] === s[j] && (j - i <= 1 || dp[i+1][j-1])) {
-    dp[i][j] = true;
-    if (j - i + 1 > maxLength) {
-    maxLength = j - i + 1;
-    startingIndex = i;
+
+    for (let left = length - 1; left >= 0; left--) {
+        for (let right = left; right < length; right++) {
+            if (s[left] === s[right] && (right - left <= 1 || dp[left+1][right-1])) {
+                dp[left][right] = true;
+                if (right - left + 1 > maxLength) {
+                    maxLength = right - left + 1;
+                    startingIndex = left;
     }}}}
-    
-    return s.slice(startingIndex, startingIndex + maxLength);
-}
+return s.slice(startingIndex, startingIndex + maxLength);
+};
