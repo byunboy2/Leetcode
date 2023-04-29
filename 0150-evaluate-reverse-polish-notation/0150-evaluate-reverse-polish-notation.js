b2f76bdf12de6fa008a09560753d6a88a73d3d68
Daniel Beyon <76236947+byunboy2@users.noreply.github.com>
2023-04-29 11:08:51 -0700

    Time: 74 ms (45.29%), Space: 45.5 MB (41.47%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    let stack = [];
+    for(let i = 0; i<tokens.length; i++){
+        let current = tokens[i];
+        if(current === "+"){
+            let a = stack.pop();
+            let b = stack.pop();
+            stack.push(b+a);
+        }
+        else if(current === "-"){
+            let a = stack.pop();
+            let b = stack.pop();
+            stack.push(b-a);
+        }
+        else if(current === "*"){
+            let a = stack.pop();
+            let b = stack.pop();
+            stack.push(b*a);
+        }
+        else if(current === "/"){
+            let a = stack.pop();
+            let b = stack.pop();
+            stack.push(parseInt(b/a));
+        }
+        else{
+            stack.push(parseInt(current));
+        }
+    }
+    return stack.pop();
+};
