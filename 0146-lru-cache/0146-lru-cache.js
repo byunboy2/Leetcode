39a3717feb070706f60b730b30000750ea8d0b88
Daniel Beyon <76236947+byunboy2@users.noreply.github.com>
2023-05-07 10:31:07 -0700

    Time: 868 ms (5.09%), Space: 127.4 MB (5.19%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.js
+++ 0146-lru-cache/0146-lru-cache.js
@@ -2,40 +2,8 @@
  * @param {number} capacity
  */
 var LRUCache = function(capacity) {
-    this.capacity = capacity;
-    this.map = new Map();
-    this.head = null;
-    this.tail = null;
-};
-
-function ListNode(key, value) {
-    this.key = key;
-    this.value = value;
-    this.prev = null;
-    this.next = null;
-}
-
-LRUCache.prototype.removeFromList = function(node) {
-    if (node.prev) {
-        node.prev.next = node.next;
-    } else {
-        this.head = node.next;
-    }
-    if (node.next) {
-        node.next.prev = node.prev;
-    } else {
-        this.tail = node.prev;
-    }
-};
-
-LRUCache.prototype.moveToFront = function(node) {
-    if (this.head === node) {
-        return;
-    }
-    this.removeFromList(node);
-    node.next = this.head;
-    this.head.prev = node;
-    this.head = node;
+  this.capacity = capacity;
+  this.cache = new Map();
 };
 
 /** 
@@ -43,12 +11,14 @@ LRUCache.prototype.moveToFront = function(node) {
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-    if (!this.map.has(key)) {
-        return -1;
-    }
-    const node = this.map.get(key);
-    this.moveToFront(node);
-    return node.value;
+  if (this.cache.has(key)) {
+    const value = this.cache.get(key);
+    this.cache.delete(key);
+    this.cache.set(key, value);
+    return value;
+  } else {
+    return -1;
+  }
 };
 
 /** 
@@ -57,25 +27,11 @@ LRUCache.prototype.get = function(key) {
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-    if (this.map.has(key)) {
-        const node = this.map.get(key);
-        node.value = value;
-        this.moveToFront(node);
-        return;
-    }
-    const node = new ListNode(key, value);
-    this.map.set(key, node);
-    if (this.head) {
-        this.head.prev = node;
-        node.next = this.head;
-        this.head = node;
-    } else {
-        this.head = node;
-        this.tail = node;
-    }
-    if (this.map.size > this.capacity) {
-        const nodeToRemove = this.tail;
-        this.removeFromList(nodeToRemove);
-        this.map.delete(nodeToRemove.key);
-    }
+  if (this.cache.has(key)) {
+    this.cache.delete(key);
+  } else if (this.cache.size >= this.capacity) {
+    const keyToDelete = this.cache.keys().next().value;
+    this.cache.delete(keyToDelete);
+  }
+  this.cache.set(key, value);
 };
