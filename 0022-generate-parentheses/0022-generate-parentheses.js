37b2c660fd0e146f3de5320428bd8290237f0501
Daniel Beyon <76236947+byunboy2@users.noreply.github.com>
2023-05-14 20:29:22 -0700

    Time: 52 ms (92.80%), Space: 43 MB (60.11%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.js
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -1,27 +1,26 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
 var generateParenthesis = function(n) {
-  let result = [];
-  let stack = [];
-
-  let generatePossibleCombinations = function(start, end) {
-    if (start === n && end === n) {
-      result.push(stack.join(''));
-      return;
-    }
-
-    if (start < n) {
-      stack.push('(');
-      generatePossibleCombinations(start + 1, end);
-      stack.pop();
-    }
-
-    if (end < start) {
-      stack.push(')');
-      generatePossibleCombinations(start, end + 1);
-      stack.pop();
-    }
-  };
-
-  generatePossibleCombinations(0, 0);
-  return result;
+    let stack = [];
+    let result =[];
+    function generatePossibleCombos(left,right){
+        if(left===n && right===n){
+            result.push(stack.join(""));
+            return;
+        }
+        if(left<n){
+            stack.push("(");
+            generatePossibleCombos(left+1,right);
+            stack.pop();
+        }
+        if(left>right){
+            stack.push(")");
+            generatePossibleCombos(left,right+1);
+            stack.pop();
+        }
+    };
+    generatePossibleCombos(0,0);
+    return result
 };
-
