91c1961e522123df84e0395ab7575534dca1792c
Daniel Beyon <76236947+byunboy2@users.noreply.github.com>
2023-05-14 21:27:02 -0700

    Time: 263 ms (87.16%), Space: 65.6 MB (52.99%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -1,14 +1,18 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
 var dailyTemperatures = function(temperatures) {
-    const stack = [];
-    const result = new Array(temperatures.length).fill(0);
-    
-    for(let i = 0; i < temperatures.length; i++){
-        while(stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
-            const j = stack.pop();
-            result[j] = i - j;
+    let result = Array(temperatures.length).fill(0);
+    let stack = [];
+    for(let i = 0; i<temperatures.length;i++){
+        while(stack.length>0 && temperatures[i] > temperatures[stack[stack.length-1]]){
+            let previousIndex = stack.pop();
+            let difference = i-previousIndex;
+            result[previousIndex]= difference;
+            
         }
         stack.push(i);
-    }
-    
+    };
     return result;
 };
