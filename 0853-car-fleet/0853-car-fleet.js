c35f92c8a6a4990eb71c304d13e5ea4e52b7a138
Daniel Beyon <76236947+byunboy2@users.noreply.github.com>
2023-05-15 15:15:29 -0700

    Time: 195 ms (98.96%), Space: 63.9 MB (98.34%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.js
+++ 0853-car-fleet/0853-car-fleet.js
@@ -1,21 +1,22 @@
-function carFleet(target, position, speed) {
-  const cars = [];
-  for (let i = 0; i < position.length; i++) {
-    cars.push({ position: position[i], speed: speed[i] });
-  }
-
-  cars.sort((a, b) => b.position - a.position);
-
-  let count = 0;
-  let maxTime = 0;
-
-  for (const car of cars) {
-    const time = (target - car.position) / car.speed;
-    if (time > maxTime) {
-      count++;
-      maxTime = time;
+/**
+ * @param {number} target
+ * @param {number[]} position
+ * @param {number[]} speed
+ * @return {number}
+ */
+var carFleet = function(target, position, speed) {
+    const pair = [];
+    for (let i = 0; i < position.length; i++) {
+      pair.push({ position: position[i], speed: speed[i] });
     }
+    pair.sort((a, b) => b.position - a.position);
+    const stack = [];
+    for (let i = 0; i < pair.length; i++) {
+      const { position: p, speed: s } = pair[i];
+      stack.push((target - p) / s);
+      if (stack.length >= 2 && stack[stack.length - 1] <= stack[stack.length - 2]) {
+        stack.pop();
+      }
+    }
+    return stack.length;
   }
-
-  return count;
-}
