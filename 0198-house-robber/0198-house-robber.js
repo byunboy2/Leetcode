eedcb5209264152e032fa47c7698ae17cd0179ad
Daniel Beyon <76236947+byunboy2@users.noreply.github.com>
2023-05-20 10:47:59 -0700

    Time: 62 ms (34.82%), Space: 42.2 MB (31.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var rob = function(nums) {
-    if(nums.length === 0) return 0;
-    let reference = Array(nums.length+1).fill(0);
-    reference[1] = nums[0];
-    
-    for(let i = 2; i<=nums.length; i++){
-        reference[i] = Math.max(reference[i-1],reference[i-2]+nums[i-1]);
+    let [rob1,rob2] = [0,0];
+    for(const num of nums){
+        temp = Math.max(rob1+num,rob2);
+        rob1 = rob2;
+        rob2 = temp;
     }
-    return reference[nums.length];
-};
+    return rob2;
+}
+
