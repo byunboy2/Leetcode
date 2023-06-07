9983f38a963222cf9421e1e56321518c412b645c
Daniel Beyon <76236947+byunboy2@users.noreply.github.com>
2023-06-07 11:38:19 -0700

    Time: 82 ms (90.65%), Space: 51.2 MB (45.95%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cs
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cs
@@ -1,21 +1,23 @@
 public class Solution {
     public string LongestPalindrome(string s) {
-        string longestString = "";
-        for(int i = 0; i < s.Length; i++) {
-            string curr = Helper(i, i, s);
-            if(curr.Length > longestString.Length) longestString = curr;
-            
-            curr = Helper(i, i + 1, s);
-            if(curr.Length > longestString.Length) longestString = curr;
+        string LongestSubString = "";
+        for(int i = 0; i<s.Length; i++){
+            string EvenString = Helper(i,i,s);
+            string OddString = Helper(i,i+1,s);
+            if(EvenString.Length>OddString.Length){
+                if(EvenString.Length > LongestSubString.Length) LongestSubString = EvenString;
+            } else {
+                if(OddString.Length > LongestSubString.Length) LongestSubString = OddString;
+            }
         }
-        return longestString;
+        return LongestSubString;
     }
-    
-    private string Helper(int left, int right, string s) {
-        while(left >= 0 && right < s.Length && s[left] == s[right]) {
+    public string Helper(int left,int right,string s){
+        while(left>=0 && right<s.Length && s[left] == s[right]){
             left--;
             right++;
         }
-        return s.Substring(left + 1, right - left - 1);
+        return s.Substring(left+1,right-left-1);
     }
 }
+
